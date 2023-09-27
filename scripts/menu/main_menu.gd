f731be690a4c004a64355e4ed1028892e1b43647
Alexander-Jordan <curtjordan93@gmail.com>
2023-09-27 10:37:57 +0200

    Finish up a working menu

---
+++ scripts/menu/main_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var main_ui_container:Control = $MarginContainer/VBoxContainer/Main
+@onready var level_select_ui_container:Control = $MarginContainer/VBoxContainer/LevelSelect
+
+signal menu_changed(to:String)
+
+func _ready():
+	$MarginContainer/VBoxContainer/Main/VBoxContainer/StartButton.grab_focus()
+
+
+func _on_start_button_pressed():
+	main_ui_container.visible = false
+	level_select_ui_container.visible = true
+	menu_changed.emit('level')
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_level_select_back_button_pressed():
+	level_select_ui_container.visible = false
+	main_ui_container.visible = true
+	menu_changed.emit('main')
+
+
+func _on_level_select_load_level_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/main.tscn")
---
+++ scripts/menu/main_menu_camera_manager.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+@onready var camera:Camera3D = $Camera3D
+@onready var main_camera_position:Node3D = $MainCameraPosition
+@onready var levels_camera_position:Node3D = $LevelsCameraPosition
+
+var target_transform:Transform3D
+var move_camera_speed:float = 4.0
+
+func _process(delta):
+	if (target_transform):
+		camera.transform = camera.transform.interpolate_with(target_transform, delta * move_camera_speed)
+
+
+func _on_menu_ui_menu_changed(to:String):
+	if (to == 'main'):
+		target_transform = main_camera_position.transform
+	elif (to == 'level'):
+		target_transform = levels_camera_position.transform
