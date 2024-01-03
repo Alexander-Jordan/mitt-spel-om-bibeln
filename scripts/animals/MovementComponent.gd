ca87609bd8f411eb239c061739bf444105c6dd82
Alexander-Jordan <curtjordan93@gmail.com>
2024-01-03 14:49:25 +0100

    Use target_position instead of direction, and slide along normal of floor with the help of raycast

--- scripts/animals/MovementComponent.gd
+++ scripts/animals/MovementComponent.gd
@@ -5,25 +5,32 @@ class_name MovementComponent
 # get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var target_position: Variant = null
-var direction: Variant = null
+var raycast: RayCast3D
+
+func _ready():
+	for child in get_children():
+		if child is RayCast3D:
+			raycast = child
+			break
+	
+	assert(raycast != null, "A RayCast3D is required as child node")
 
 func _physics_process(delta):
 	# add gravity
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
-	if direction is Vector3:
-		velocity = Vector3(direction.x, velocity.y, direction.z) * movement_speed
-		look_at(global_position + direction, Vector3.UP, true)
+	if target_position is Vector3:
+		var n: Vector3 = raycast.get_collision_normal()
+		if n.length_squared() < 0.001:
+			n = Vector3(0, 1, 0)
+		velocity = (target_position - transform.origin).slide(n).normalized() * movement_speed
+		look_at(transform.origin + velocity, Vector3.UP, true)
 	
 	move_and_slide()
 
 func _on_navigation_component_next_path_position(next_path_position: Variant):
 	self.target_position = next_path_position
-	
-	if next_path_position is Vector3:
-		self.direction = global_position.direction_to(next_path_position)
-		return
-	
-	self.direction = null
-	velocity = Vector3.ZERO
+	# clear velocity if no next_path_position
+	if next_path_position == null:
+		velocity = Vector3.ZERO
