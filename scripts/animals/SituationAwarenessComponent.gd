edac69cb258fa1a332a5c2a11dbd3ad009e24def
Alexander-Jordan <curtjordan93@gmail.com>
2024-01-08 07:27:33 +0100

    More finalized version of the SituationAwarenessComponent

--- scripts/animals/SituationAwarenessComponent.gd
+++ scripts/animals/SituationAwarenessComponent.gd
@@ -2,7 +2,6 @@ extends Node3D
 class_name SituationAwarenessComponent
 
 var area_close: Area3D
-var area_far: Area3D
 var animal_objectives: AnimalObjectives
 
 @export var player_is_close_objective: Objective
@@ -10,37 +9,24 @@ var animal_objectives: AnimalObjectives
 func _ready():
 	for child in get_children():
 		if child is Area3D:
-			if child.name == 'AreaClose':
-				area_close = child
-				continue
-			if child.name == 'AreaFar':
-				area_far = child
-				continue
+			area_close = child
 		if child is AnimalObjectives:
 			animal_objectives = child
 	
-	assert(area_close != null, '"AreaClose" (Area3D) is required as child node')
-	assert(area_far != null, '"AreaFar" (Area3D) is required as child node')
+	assert(area_close != null, 'Area3D is required as child node')
 	assert(animal_objectives != null, 'AnimalObjectives is required as child node')
 	
-	for area in [area_close, area_far]:
-		area.body_entered.connect(func(body: Node3D): _area3D_body_entered(body, area))
-		area.body_exited.connect(func(body: Node3D): _area3D_body_exited(body, area))
+	area_close.body_entered.connect(_area3D_body_entered)
+	area_close.body_exited.connect(_area3D_body_exited)
 		
 
-func _area3D_body_entered(body: Node3D, area: Area3D):
-	if area == area_close:
-		if body.name == 'player':
-			player_entered_close()
-	if area == area_far:
-		pass
+func _area3D_body_entered(body: Node3D):
+	if body.name == 'player':
+		player_entered_close()
 
-func _area3D_body_exited(body: Node3D, area: Area3D):
-	if area == area_close:
-		if body.name == 'player':
-			player_exited_close()
-	if area == area_far:
-		pass
+func _area3D_body_exited(body: Node3D):
+	if body.name == 'player':
+		player_exited_close()
 
 func player_entered_close():
 	if player_is_close_objective != null:
--- scripts/objective/IdleObjective.gd
+++ scripts/objective/IdleObjective.gd
@@ -1,6 +1,9 @@
 extends Objective
 class_name IdleObjective
 
+@export var min_time: int = 1
+@export var max_time: int = 10
+
 var state_component: StateComponent
 var state: String = 'idle'
 
@@ -17,4 +20,4 @@ func try_to_set_required_components(nodes_to_search: Array[Node]) -> bool:
 # execute the objective
 func execute():
 	state_component.set_state(self.state)
-	start_random_time_from_range(1, 10)
+	start_random_time_from_range(min_time, max_time)
