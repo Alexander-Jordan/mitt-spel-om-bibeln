e7ebd17263abe31c05b83db452ad47292b604a16
Alexander Jordan <curtjordan93@gmail.com>
2024-03-26 11:13:00 +0100

    Load levels through paths instead of ScenePackage

--- scripts/level/Level.gd
+++ scripts/level/Level.gd
@@ -4,4 +4,4 @@ class_name Level
 @export var name:String = 'Level Name'
 @export var description:String = 'Level description'
 @export var image:Texture2D
-@export var scene:PackedScene
+@export var path:String
