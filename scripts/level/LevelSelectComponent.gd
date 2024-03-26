c753c9181c141c141a3905daf71b6904efa4acf8
Alexander Jordan <curtjordan93@gmail.com>
2024-03-26 09:19:26 +0100

    Add LevelSelectComponent, Level resource, and put it together to make it possible to choose level

---
+++ scripts/level/LevelSelectComponent.gd
@@ -0,0 +1,45 @@
+extends Node
+class_name LevelSelectComponent
+
+@export var levels:Array[Level]
+var current_level:Level
+
+@onready var name_label:Label = $HBoxContainer2/VBoxContainer/LevelName
+@onready var image_texture_rect:TextureRect = $HBoxContainer2/VBoxContainer/HBoxContainer/LevelImage
+@onready var description_label:Label = $HBoxContainer2/VBoxContainer/HBoxContainer/LevelDescription
+
+func _ready():
+	if levels.is_empty():
+		return
+	current_level = levels[0]
+	level_changed()
+
+func level_changed():
+	name_label.text = current_level.name
+	description_label.text = current_level.description
+	image_texture_rect.texture = current_level.image
+
+func next_level():
+	if levels.is_empty():
+		return
+	var next_index = levels.find(current_level) + 1
+	next_index = 0 if next_index >= levels.size() else next_index
+	current_level = levels[next_index]
+	level_changed()
+
+func previous_level():
+	if levels.is_empty():
+		return
+	var previous_index = levels.find(current_level)
+	previous_index = levels.size() - 1 if previous_index <= 0 else previous_index - 1
+	current_level = levels[previous_index]
+	level_changed()
+
+func _on_previous_button_pressed():
+	previous_level()
+
+func _on_next_button_pressed():
+	next_level()
+
+func _on_load_level_button_pressed():
+	pass # Replace with function body.
