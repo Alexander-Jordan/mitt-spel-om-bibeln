f3d937302fff16c95f9d980b69b208e64814d9d4
Alexander Jordan <curtjordan93@gmail.com>
2024-03-31 15:59:03 +0200

    Fix SightInteractionComponent a bit

--- scripts/player/SightInteractorComponent.gd
+++ scripts/player/SightInteractorComponent.gd
@@ -1,34 +1,38 @@
 extends RayCast3D
 class_name SightInteractorComponent
 
-var collider:Object = null
 var interactor:SituationAwarenessComponent
 
 signal play_audio(audio:AudioStream)
 signal interact_changed(can_interact:bool)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	if collider != null and collider is CharacterBody3D and interactor == null:
-		for child in collider.get_children():
+	# do some action when trying to interact with an interactor
+	if interactor != null and Input.is_action_just_pressed('interact'):
+		play_audio.emit(interactor.answers_to)
+		interactor.listen_to_call(self.global_position)
+
+func _physics_process(delta):
+	if is_colliding():
+		# stop here if the interactor has already been set from this collision
+		if interactor != null:
+			return
+		
+		# go through all components, stop if interactor is found
+		for child in get_collider().get_children():
 			if child is SituationAwarenessComponent:
 				if interactor != child:
 					interactor = child
 					interact_changed.emit(true)
 				break
-	
-	if interactor != null and Input.is_action_just_pressed('interact'):
-		play_audio.emit(interactor.answers_to)
-		interactor.listen_to_call(self.global_position)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	if not is_colliding() and collider != null:
-		collider = null
+		# no interactor found, skip next time the ray collides with this object
+		if interactor == null:
+			add_exception(get_collider())
+	# transition from collision with an interactor
+	elif interactor != null:
+		# reset interactor and emit
 		interactor = null
 		interact_changed.emit(false)
-	if is_colliding():
-		collider = get_collider()
