ac669d5b7999ca54055d31650f80d3133d5e3919
Alexander Jordan <curtjordan93@gmail.com>
2024-04-24 11:16:00 +0200

    Add credits, with 2 avatars

--- scripts/menu/main_menu_camera_manager.gd
+++ scripts/menu/main_menu_camera_manager.gd
@@ -3,17 +3,20 @@ extends Node3D
 @onready var camera:Camera3D = $Camera3D
 @onready var main_camera_position:Node3D = $MainCameraPosition
 @onready var levels_camera_position:Node3D = $LevelsCameraPosition
+@onready var credits_camera_position:Node3D = $CreditsCameraPosition
 
 var target_transform:Transform3D
 var move_camera_speed:float = 4.0
 
 func _process(delta):
-	if (target_transform):
+	if target_transform:
 		camera.transform = camera.transform.interpolate_with(target_transform, delta * move_camera_speed)
 
 
 func _on_menu_ui_menu_changed(to:String):
-	if (to == 'main'):
+	if to == 'main':
 		target_transform = main_camera_position.transform
-	elif (to == 'level'):
+	elif to == 'level':
 		target_transform = levels_camera_position.transform
+	elif to == 'credits':
+		target_transform = credits_camera_position.transform
